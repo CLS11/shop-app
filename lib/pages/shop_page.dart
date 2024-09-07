447778f72775f51e813f8601174929d11b6b91e9
crystall.101600 <crystall.101600@gmail.com>
2024-09-07 06:33:40 +0000

    Shop page with a drawer.

---
+++ lib/pages/shop_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ShopPage extends StatelessWidget {
+  const ShopPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Shop Page"),
+      ),
+      drawer: Drawer(
+
+      ),
+    );
+  }
+}
