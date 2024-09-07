1ecb3c044517813b47ecf633bd4518851617cccf
crystall.101600 <crystall.101600@gmail.com>
2024-09-07 11:28:57 +0000

    All the UI details about the product along with adding to Cart function.

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,22 +1,65 @@
-import 'package:flutter/materials.dart';
+import 'package:flutter/material.dart';
+import 'package:ecommerce/components/button.dart';
+import 'package:ecommerce/components/list_tile.dart';
+import 'package:ecommerce/pages/intro_page.dart';
 
-class Drawer extends StatelessWidget{
-  const ({super.key});
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return Drawer(
+  Widget build(BuildContext context) {
+    return Drawer(  
       backgroundColor: Theme.of(context).colorScheme.surface,
       child: Column(
-      children: [
-      //drawer header: logo
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              // Drawer header: logo
+              DrawerHeader(
+                child: Center(
+                  child: Icon(
+                    Icons.shop,  
+                    size: 75,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 25),
+              // Shop tile
+              MyListTile(
+                text: "Shop",
+                icon: Icons.home,
+                onTap: () => Navigator.pop(context),
+              ),
+              // Cart tile
+              MyListTile(
+                text: "Cart",
+                icon: Icons.shopping_cart,
+                onTap: () {
+                  // Pop the drawer first
+                  Navigator.pop(context);
 
-      //shop title
-
-      //cart title
-
-      
-      ],
+                  // Go to the cart page
+                  Navigator.pushNamed(context, '/cart_page');
+                },
+              ),
+              // Exit tile
+              Padding(
+                padding: const EdgeInsets.only(bottom: 25.0),
+                child: MyListTile(
+                  text: "Exit",
+                  icon: Icons.logout,
+                  onTap: () => Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => const IntroPage()),
+                    (route) => false,  
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
