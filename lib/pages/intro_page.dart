5fc02d275cc155fa2839721f61ed581f69019ad6
crystall.101600 <crystall.101600@gmail.com>
2024-09-07 06:32:33 +0000

    Introduction page with a navigator button to shop page.

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/components/button.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(
+              Icons.shopping_bag,
+              size: 75,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            const SizedBox(height: 25),
+
+            //title
+            const Text(
+              "Minimal Shop",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            //subtitle
+            Text(
+              "Premium Quality Products",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.inversePrimary,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            //button to add
+            Button(
+              onTap: () => Navigator.pushNamed(context, '/shop_page'),
+              child: const Icon(Icons.arrow_forward),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
