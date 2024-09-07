166b52df556cd786692be2e075a07ebcc76fda13
crystall.101600 <crystall.101600@gmail.com>
2024-09-07 12:12:07 +0000

    Routing to different pages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:ecommerce/models/shop.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce/pages/intro_page.dart';
 import 'package:ecommerce/themes/light_mode.dart';
 import 'package:ecommerce/pages/shop_page.dart';
+import 'package:ecommerce/pages/cart_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const ShopApp());
+  runApp(
+    ChangeNotifierProvider(create: (context) => Shop(),
+    child: const ShopApp(),
+    ),
+  );
 }
 
 class ShopApp extends StatelessWidget {
@@ -19,6 +26,7 @@ class ShopApp extends StatelessWidget {
         routes: {
           '/intro_page': (context) => const IntroPage(),
           '/shop_page': (context) => const ShopPage(),
+          '/cart_page': (context) => const CartPage(),
         });
   }
 }
