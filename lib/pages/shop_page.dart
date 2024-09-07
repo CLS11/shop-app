416450b448056dfb477b684cdfac8577b09c8fdd
crystall.101600 <crystall.101600@gmail.com>
2024-09-07 11:59:48 +0000

    Shop page with a list view builder with navigation to product tile.

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,10 +1,17 @@
+import 'package:ecommerce/components/product_tile.dart';
+import 'package:ecommerce/models/shop.dart';
 import 'package:flutter/material.dart';
+import 'package:ecommerce/components/drawer.dart';
+import 'package:provider/provider.dart';
 
 class ShopPage extends StatelessWidget {
   const ShopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //accessing products in the shop
+    final products = context.watch<Shop>().shop;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -12,8 +19,38 @@ class ShopPage extends StatelessWidget {
         foregroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Shop Page"),
       ),
-      drawer: Drawer(
+      drawer: const MyDrawer(),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: ListView(
+        children: [
+          const SizedBox(height: 25),
 
+          //shop subtitle
+          Center(
+            child: Text(
+              "Pick from a selected list of premium products.",
+              style: TextStyle(
+              color: Theme.of(context).colorScheme.inversePrimary
+              ),
+            ),
+          ),
+
+          //productlist
+          SizedBox(
+            height: 550,
+            child: ListView.builder(
+                itemCount: products.length,
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.all(15),
+                itemBuilder: (context, index) {
+                  //get products from shop
+                  final product = products[index];
+
+                  //return as the product tile UI
+                  return MyProductTile(product: product);
+                }),
+          ),
+        ],
       ),
     );
   }
