e64eab73a2b9c310d6ad31f376a4bdc3c1f273d2
crystall.101600 <crystall.101600@gmail.com>
2024-09-07 12:01:57 +0000

    UI for cart page.

---
+++ lib/pages/cart_page.dart
@@ -0,0 +1,20 @@
+import 'package:ecommerce/components/drawer.dart';
+import 'package:flutter/material.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Cart Page"),
+      ),
+      drawer: const MyDrawer(),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+    );
+  }
+}
