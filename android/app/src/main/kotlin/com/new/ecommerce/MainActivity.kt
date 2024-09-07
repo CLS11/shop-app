4d2c7f1f8f355474cde767a916a7afc85d7a4cb3
crystall.101600 <crystall.101600@gmail.com>
2024-09-07 07:38:09 +0000

    add

--- android/app/src/main/kotlin/com/new/ecommerce/MainActivity.kt
+++ android/app/src/main/kotlin/com/new/ecommerce/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapp
+package com.new.ecommerce
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ lib/components/drawer.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/materials.dart';
+
+class Drawer extends StatelessWidget{
+  const ({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Column(
+      children: [
+      //drawer header: logo
+
+      //shop title
+
+      //cart title
+
+      
+      ],
+      ),
+    );
+  }
+}
